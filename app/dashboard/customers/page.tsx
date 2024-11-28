8534cbead25b370da289422028cdde96bca5b2f4
Muhammed Arshid <open.arshid@gmail.com>
2024-11-28 15:13:40 +0530

    basic

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,8 @@
+import Image from 'next/image';
+
+export default function Page() {
+    return <div className="bg-green-300">
+      <h1 className="font-bold">Arshid</h1>
+      <p>Customers Page</p>
+      </div>
+  }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+import { Abel } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    subsets: ['latin'],
+    weight: '400'
+});
+
+
+export const abel = Abel({
+    subsets: ['latin'],
+    weight: '400'
+});
